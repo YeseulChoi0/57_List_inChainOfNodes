572195f601857f768b2415e9f0520bcf7b5e2ef8
YeseulChoi0 <ychoi00@stuy.edu>
2019-03-26 14:17:53 -0400

    made the code more efficient using strategies we talked about in class

--- List_inChainOfNodes.java
+++ List_inChainOfNodes.java
@@ -53,25 +53,26 @@ public class List_inChainOfNodes{
      public boolean addAsHead( Object val) {
         Node head = new Node(val, headReference);
         headReference = head;
-
-
-       return true;
+        return true;
      }
 
-     public boolean set(int index, Object val){
+     private Node findReferenceAtIndex(int index){
        Node referenceAtIndex = headReference;
-       for (int counter = 0; counter < index; counter++){
-         referenceAtIndex = referenceAtIndex.getReferenceToNextNode();
+       int counter = 0;
+       for (counter = 0, referenceAtIndex = headReference; counter < index; counter++, referenceAtIndex = referenceAtIndex.getReferenceToNextNode()){
+         ;
        }
+       return referenceAtIndex;
+     }
+
+     public Object set(int index, Object val){
+       Node referenceAtIndex = findReferenceAtIndex(index);
        referenceAtIndex.setValueOfNode(val);
-       return true;
+       return referenceAtIndex.getCargoReference();
      }
 
      public Object get(int index){
-       Node referenceAtIndex = headReference;
-       for (int counter = 0; counter < index; counter++){
-         referenceAtIndex = referenceAtIndex.getReferenceToNextNode();
-       }
+       Node referenceAtIndex = findReferenceAtIndex(index);
        return referenceAtIndex.getCargoReference();
      }
 
@@ -79,15 +80,18 @@ public class List_inChainOfNodes{
        if (index == 0){
          addAsHead(val);
        }else{
-         Node prevReference = headReference;
-         for (int counter = 0; counter < index-1; counter++){
-           prevReference = prevReference.getReferenceToNextNode();
-         }
+         Node prevReference = findReferenceAtIndex(index - 1);
+         // Node prevReference = headReference;
+         // for (int counter = 0; counter < index-1; counter++){
+         //   prevReference = prevReference.getReferenceToNextNode();
+         //}
+
          Node referenceAtIndex = prevReference.getReferenceToNextNode();
          Node newNode = new Node(val, referenceAtIndex);
          prevReference.setReferenceToNextNode(newNode);
        }
      }
+
 
      public Object remove(int index){
        Object returner = 0;
@@ -95,10 +99,11 @@ public class List_inChainOfNodes{
          returner = headReference.getCargoReference();
          headReference = headReference.getReferenceToNextNode();
        }else if (index == size() - 1){
-         Node secondToLast = headReference;
-         for (int counter = 0; counter < index - 2; counter++){
-           secondToLast = secondToLast.getReferenceToNextNode();
-         }
+         Node secondToLast = findReferenceAtIndex(index - 2);
+         // Node secondToLast = headReference;
+         // for (int counter = 0; counter < index - 2; counter++){
+         //   secondToLast = secondToLast.getReferenceToNextNode();
+         // }
          Node newLastReference = secondToLast.getReferenceToNextNode();
          returner = newLastReference.getReferenceToNextNode().getCargoReference();
          Node newLast = new Node(newLastReference.getCargoReference());
@@ -111,16 +116,19 @@ public class List_inChainOfNodes{
          // returner = newLastReference.getReferenceToNextNode().getCargoReference();
          // newLastReference.setReferenceToNextNode(null);
          // System.out.println(newLastReference);
+
        }else{
-         Node refBefore = headReference;
-         for (int counter = 0; counter < index - 1; counter++){
-           refBefore = refBefore.getReferenceToNextNode();
-         }
+         Node refBefore = findReferenceAtIndex(index - 1);
+         // Node refBefore = headReference;
+         // for (int counter = 0; counter < index - 1; counter++){
+         //   refBefore = refBefore.getReferenceToNextNode();
+         // }
          returner = refBefore.getReferenceToNextNode().getCargoReference();
-         Node refAfter = headReference;
-         for (int counter = 0; counter < index + 1; counter++){
-           refAfter = refAfter.getReferenceToNextNode();
-         }
+         Node refAfter = findReferenceAtIndex(index+1);
+         // Node refAfter = headReference;
+         // for (int counter = 0; counter < index + 1; counter++){
+         //   refAfter = refAfter.getReferenceToNextNode();
+         // }
          refBefore.setReferenceToNextNode(refAfter);
        }
        return returner;
